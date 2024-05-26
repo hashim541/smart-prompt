0712a8574e3ac91c1a615e82f91a405bf186efc0
hashim <hashim00541x@gmail.com>
2024-05-26 20:25:32 +0530

    updating update-prompt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from "react"
+import { useEffect, useState, Suspense } from "react"
 import { useSession } from "next-auth/react"
 import { useRouter, useSearchParams } from "next/navigation"
 import Form from '@components/Form'
